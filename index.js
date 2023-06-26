09afcaf943ab607737eab294aea98e77d9dbf7d4
Izzysonne <123116676+Izzysonne@users.noreply.github.com>
2023-06-26 00:25:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ const displayContainer = document.getElementById("display-container")
         choicesContainer.style.display = "none"
         displayContainer.style.display = "flex"
         if(screenSize.matches){
-            rulesBtn.style.marginTop = "18em"
+            rulesBtn.style.marginTop = "14em"
             }
         playersBtn.classList.add(`${playerChoice}`)
         setTimeout(function(){
